3a76e6a3c988de2c8a9923af8944495621d8b1b2
Tim Crisp <timothycrisp@gmail.com>
2022-04-29 17:46:30 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,34 @@
+twiOne = document.querySelector('.twi-one')
+
+twiOne.textContent = 
+
+    'This week I had a phone interview. I applied for ten jobs. I finished my portfolio site.' 
+
+twiTwo = document.querySelector('.twi-two')
+
+twiTwo.textContent = 
+
+    'I created directories for Arrays and Dom learning, with HTML, CSS, and JavaScript. I used array iterator methods to access and display data from a data array of 25 albums, that I created.'
+
+twiThree = document.querySelector('.twi-three')
+
+twiThree.textContent = 
+
+    'This week I wrote more for loops that any other week.'
+
+twiFour = document.querySelector('.twi-four')
+
+twiFour.textContent = 
+
+    'This week I used the Dom to access, store, and modify HTML and CSS elements in JavaScript. I made GitHub commits each day, and I finally made a list of the hottest array iterator methods.'
+
+twiFive = document.querySelector('.twi-five')
+
+twiFive.textContent = 
+
+    'This weekend I will earn two stickers. This weekend I will have one day free of Github commits.'
+
+date = document.querySelector('.date')
+console.log(date)
+
+date.textContent = 'Friday April, 29'
