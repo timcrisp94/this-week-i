0ffe85eecc4359faf3d4ecd7948c0aa78f5397a1
Tim Crisp <timothycrisp@gmail.com>
2022-05-07 07:03:51 -0500

    add html and js for friday may 6

---
+++ 4-29.js
@@ -0,0 +1,32 @@
+const date = document.querySelector('.date')
+date.textContent = 'Friday April, 29'
+
+const twiOne = document.querySelector('.twi-one')
+twiOne.textContent = 
+
+    'This week I had a phone interview. I applied for ten jobs. I finished my portfolio site.' 
+
+const twiTwo = document.querySelector('.twi-two')
+twiTwo.textContent = 
+
+    'I created directories for Arrays and Dom learning, with HTML, CSS, and JavaScript. I used array iterator methods to access and display data from a data array of 25 albums, that I created.'
+
+const twiThree = document.querySelector('.twi-three')
+twiThree.textContent = 
+
+    'This week I wrote more for loops that any other week.'
+
+const twiFour = document.querySelector('.twi-four')
+twiFour.textContent = 
+
+    'This week I used the Dom to access, store, and modify HTML and CSS elements in JavaScript. I made GitHub commits each day, and I finally made a list of the hottest array iterator methods.'
+
+const twiFive = document.querySelector('.twi-five')
+twiFive.textContent = 
+
+    'This weekend I will earn two stickers. This weekend I will have one day free of Github commits.'
+
+const signOff = document.querySelector('.sign-off')
+signOff.textContent = 
+
+    ':)'
--- app.js
+++ app.js
@@ -1,34 +1,32 @@
-twiOne = document.querySelector('.twi-one')
+const date = document.querySelector('.date')
+date.textContent = 'Friday, May 6'
 
+const twiOne = document.querySelector('.twi-one')
 twiOne.textContent = 
 
-    'This week I had a phone interview. I applied for ten jobs. I finished my portfolio site.' 
+    'This week I received a technical interview challenge for a great job. I applied for ten other jobs. I improved my resume and update my portfolio site.' 
 
-twiTwo = document.querySelector('.twi-two')
+const twiTwo = document.querySelector('.twi-two')
+    twiTwo.textContent = 
 
-twiTwo.textContent = 
+    'I started TypeScript learning modules, and learned about Types, Classes, and Interfaces.'
 
-    'I created directories for Arrays and Dom learning, with HTML, CSS, and JavaScript. I used array iterator methods to access and display data from a data array of 25 albums, that I created.'
-
-twiThree = document.querySelector('.twi-three')
-
+const twiThree = document.querySelector('.twi-three')
 twiThree.textContent = 
 
-    'This week I wrote more for loops that any other week.'
+    'This week I built a browser-based game in JavaScript and started to build a tic-tac-toe game.'
 
-twiFour = document.querySelector('.twi-four')
-
+const twiFour = document.querySelector('.twi-four')
 twiFour.textContent = 
 
-    'This week I used the Dom to access, store, and modify HTML and CSS elements in JavaScript. I made GitHub commits each day, and I finally made a list of the hottest array iterator methods.'
+    'This week I created user-stories, and started to build a data structure for an "album-a-day" app.'
 
-twiFive = document.querySelector('.twi-five')
-
+const twiFive = document.querySelector('.twi-five')
 twiFive.textContent = 
 
     'This weekend I will earn two stickers. This weekend I will have one day free of Github commits.'
 
-date = document.querySelector('.date')
-console.log(date)
+const signOff = document.querySelector('.sign-off')
+signOff.textContent = 
 
-date.textContent = 'Friday April, 29'
+    ':)'
